f245e4096fbdc78e466633f61a3a04f6d9f6112c
jbrooks6 <jbrooks6@student.strode-college.ac.uk>
2020-11-08 19:51:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Dice face 5 problem
+# Main program 
+def Output5():
+  print("oooooooooo")
+  print("o        o")
+  print("o  #   # o")
+  print("o    #   o")
+  print("o  #   # o")
+  print("o        o")
+  print("oooooooooo")
+Output5()
